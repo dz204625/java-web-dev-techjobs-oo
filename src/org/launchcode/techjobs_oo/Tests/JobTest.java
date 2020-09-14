8309ca9b2104ce34ec5fda0ced15eb32193ff781
Dan Zhu <zhudan0513@hotmamil.comn>
2020-09-13 21:41:31 -0500

    fixed the unit testing

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -28,30 +28,30 @@ public class JobTest {
     @Test
     public void testToStringStartEndWithNewLine(){
         Job myJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        assertEquals( "\nid: 1\nname: Product tester\nemployer: ACME\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
+        assertEquals( "\nid: 4\nname: Product tester\nemployer: ACME\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
     }
     @Test
     public void testToStringEachFieldHasData(){
         Job myJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        assertEquals( "\nid: 1\nname: Product tester\nemployer: ACME\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
+        assertEquals( "\nid: 11\nname: Product tester\nemployer: ACME\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
     }
 
     @Test
     public void testToStringWithEmptyName(){
         Job myJob1 = new Job("", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        assertEquals( "\nid: 1\nname: Data not available\nemployer: ACME\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
+        assertEquals( "\nid: 5\nname: Data not available\nemployer: ACME\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
     }
 
     @Test
     public void testToStringWithEmptyEmployer(){
         Job myJob1 = new Job("Product tester", new Employer(""), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        assertEquals( "\nid: 1\nname: Product tester\nemployer: Data not available\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
+        assertEquals( "\nid: 7\nname: Product tester\nemployer: Data not available\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
     }
 
     @Test
     public void testToStringWithEmptyLocation(){
         Job myJob1 = new Job("Product tester", new Employer("ACME"), new Location(""), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        assertEquals( "\nid: 1\nname: Product tester\nemployer: ACME\nlocation: Data not available\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
+        assertEquals( "\nid: 10\nname: Product tester\nemployer: ACME\nlocation: Data not available\npositionType: Quality control\ncoreCompetency: Persistence\n" , myJob1.toString());
     }
 
     @Test
@@ -63,7 +63,7 @@ public class JobTest {
     @Test
     public void testToStringWithEmptyCoreCompetency(){
         Job myJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency(""));
-        assertEquals( "\nid: 1\nname: Product tester\nemployer: ACME\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Data not available\n" , myJob1.toString());
+        assertEquals( "\nid: 12\nname: Product tester\nemployer: ACME\nlocation: Desert\npositionType: Quality control\ncoreCompetency: Data not available\n" , myJob1.toString());
     }
 
 
